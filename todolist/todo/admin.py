d1f94162eac14a5bcc40377d689db0bd8468571e
vlad <vladlen13372288800@gmail.com>
2026-06-30 14:30:37 +0300

    Upgraded

--- todolist/todo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "is_done", "created_at")
-    list_filter = ("is_done",)
-    search_fields = ("title",)
