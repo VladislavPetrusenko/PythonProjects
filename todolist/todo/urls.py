0bd87f606e1591b605bc747f54866d291aa9067e
vlad <vladlen13372288800@gmail.com>
2026-08-25 18:01:38 +0300

    updated

--- todolist/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.task_list, name="task_list"),
-    path("add/", views.task_create, name="task_create"),
-    path("<int:pk>/toggle/", views.task_toggle_done, name="task_toggle_done"),
-    path("<int:pk>/delete/", views.task_delete, name="task_delete")
-]
